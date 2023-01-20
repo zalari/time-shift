afde35d7d848cde5d92e8066b20d364b48c9e26f
David Enke <david.enke@zalari.de>
2023-01-20 13:52:25 +0100

    added fix for safari

--- packages/core/src/utils/form.utils.ts
+++ packages/core/src/utils/form.utils.ts
@@ -10,8 +10,19 @@ export const unwrapFormElements = (element: any): Array<EditableInterface<any>> 
   return [element] as unknown as Array<EditableInterface<any>>;
 };
 
+// fallback for browsers without support for ElementInternals and `form.elements`
+export const collectFormElements = (element: Element): Element[] => {
+  const isFormAssociated = 'formAssociated' in element.constructor;
+  const isSlotElement = element instanceof HTMLSlotElement;
+  if (isFormAssociated) return [element];
+  const children = isSlotElement ? element.assignedElements() : element.children;
+  return Array.from(children).flatMap(child => collectFormElements(child));
+};
+
 export const getFormElements = (form: HTMLFormElement): Array<EditableInterface<any>> => {
-  return Array.from(form.elements).flatMap(element => unwrapFormElements(element));
+  const supportElementInternals = typeof ElementInternals !== 'undefined';
+  const elements = supportElementInternals ? form.elements : collectFormElements(form);
+  return Array.from(elements).flatMap(element => unwrapFormElements(element));
 };
 
 export const collectDataFromElements = <T>(
